f01ae1d0684920601f6cfa240b6d7776b7fcee00
Abhishek Mishra <hackwithabhishek@gmail.com>
2021-02-17 09:40:32 +0530

    Update 7.FindPermutation.cpp

--- Recursion and Backtracking/7.FindPermutation.cpp
+++ Recursion and Backtracking/7.FindPermutation.cpp
@@ -1,1 +1,23 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+void Permutation(string s,int l,int r){
+    if(l==r){
+        cout<<s<<endl;
+    }
+    else{
+        
+        for(int i=l;i<=r;i++)
+        {
+           swap(s[1],s[i]);
+           Permutation(s,l+1,r);
+           swap(s[1],s[i]);
+        }
+    }
+}
+int main(){
+    string s;
+    cin>>s;
+    int n=s.size();
+    Permutation(s,0,n-1);
+    return 0;
+}
