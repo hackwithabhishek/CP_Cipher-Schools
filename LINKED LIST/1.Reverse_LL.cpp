f438a712cae739e0a21c0cc2fef863cb3150f216
Abhishek Mishra <hackwithabhishek@gmail.com>
2021-02-18 18:34:17 +0530

    Create 1.Reverse_LL.cpp

---
+++ LINKED LIST/1.Reverse_LL.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+struct Node {
+  int data;
+  struct Node* next;
+  Node(int data)
+  {
+    this->data = data;
+    next = NULL;
+  }
+};
+struct LinkedList {
+  Node* head;
+  LinkedList()
+  {
+    head = NULL;
+  }
+  void reverse()
+  {
+    Node* current = head;
+    Node *prev = NULL, *next = NULL;
+    while (current != NULL) {
+      next = current->next;
+      current->next = prev;
+      prev = current;
+      current = next;
+    }
+    head = prev;
+  }
+  void print()
+  {
+    struct Node* temp = head;
+    while (temp != NULL) {
+      cout << temp->data << " ";
+      temp = temp->next;
+    }
+  }
